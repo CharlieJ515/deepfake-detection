fe16722d05d99fa93c5c94538ded24727df27cf9
Charlie Jang <charlie.jang515@gmail.com>
2025-11-06 10:01:41 +0900

    utility functions for splitting tarballs, finding tarballs under directory, expanding list of brace patterns

---
+++ datasets/utils.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import webdataset as wds
+import braceexpand
+
+def split_tarball(input_tar: Path, output_pattern:str, count: int):
+    tar_path = str(input_tar.as_posix())
+    print(tar_path)
+    with wds.writer.ShardWriter(output_pattern, maxcount=count) as sink:
+        for sample in wds.compat.WebDataset(tar_path):
+            sink.write(sample)
+
+def find_tarballs(root_dir: Path) -> list[str]:
+    root = Path(root_dir).expanduser().absolute()
+    tar_paths = sorted(p.as_posix() for p in root.glob("*.tar"))
+    return tar_paths
+
+def expand_brace_patterns(patterns: list[str]) -> list[str]:
+    out: list[str] = []
+    for p in patterns:
+        out.extend(list(braceexpand.braceexpand(p)))
+    return out
