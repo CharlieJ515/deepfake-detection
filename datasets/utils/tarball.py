a8982e9214b9d8208bfcc024cac27690346faa1c
Charlie Jang <charlie.jang515@gmail.com>
2025-11-15 22:07:03 +0900

    helper function for creating shards from images

--- datasets/utils/tarball.py
+++ datasets/utils/tarball.py
@@ -16,3 +16,27 @@ def find_tarballs(root_dir: Path) -> list[str]:
     root = Path(root_dir).expanduser().absolute()
     tar_paths = sorted(p.as_posix() for p in root.glob("*.tar"))
     return tar_paths
+
+
+def create_shards(images_dir: Path, output_pattern: str, maxcount: int = 1000):
+    Path(output_pattern).parent.mkdir(parents=True, exist_ok=True)
+
+    sink = wds.writer.ShardWriter(
+        output_pattern,
+        maxcount=maxcount,
+        opener=lambda f: open(f, "wb"),
+    )
+    for img in images_dir.iterdir():
+        if not img.is_file():
+            continue
+        suffix = img.suffix.lower().lstrip(".")
+        if suffix not in ["jpg", "jpeg", "png", "webp"]:
+            continue
+
+        sample = {
+            "__key__": str(img.stem),
+            suffix: img.read_bytes(),
+        }
+
+        sink.write(sample)
+    sink.close()
